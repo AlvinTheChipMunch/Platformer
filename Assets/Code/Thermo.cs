f0e5aa911fbeb74ff821d0316877f2122f84f304
AlvinTheChipMunch <quyngyen24012010@gmail.com>
2025-11-04 17:27:56 +0700

    Add files

--- Assets/Code/Thermo.cs
+++ Assets/Code/Thermo.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Thermo : MonoBehaviour
@@ -7,6 +10,8 @@ public class Thermo : MonoBehaviour
     public float heat = 37;
     public float healthDepleteRate = 2f;
     public Health health;
+    public bool nearfire;
+    public TextMeshProUGUI tempText;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,22 +21,37 @@ public class Thermo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        heat -= Time.deltaTime;
+        if (nearfire)
+        {
+            heat += Time.deltaTime;
+        }
+        else
+        {
+            heat -= Time.deltaTime;
+        }
         if (heat <= 25)
         {
             health.LoseHealth(0.001f);
         }
+        if (heat > 40)
+        {
+            health.LoseHealth(0.001f);
+        }
+        tempText.text = Math.Round(heat+1) + "";
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Fire")
         {
-            heat += Time.deltaTime * 3;
-            if (heat >= 40)
-            {
-                health.LoseHealth(100);
-            }
+         nearfire = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Fire")
+        {
+            nearfire = false;
         }
     }
 }
